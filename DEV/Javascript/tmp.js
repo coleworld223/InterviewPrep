a3973af859c2d65b06dab3d59f801eb51a43a852
anuj <anujanand223@gmai.com>
2024-06-12 16:27:31 +0530

    added basic js

---
+++ DEV/Javascript/tmp.js
@@ -0,0 +1,30 @@
+console.log ("anuj");
+
+// Variables 
+// const collegId = 5; // once defined should be initialised and cannot be changed later / block scoped
+let x = 5; // can be changed later / block scoped / cannot be redeclared
+var y = 5; // can be changed later / global scoped / can be redeclared
+
+// collegId = 5; -> gives error ("can not assign to a constant variable")
+// let x = 7; -> gives an error ("can not re initialise a let") 
+var y = 7; // no error
+
+// Data Types 
+
+/*
+    number
+    bingInt
+    string
+    boolean
+    undefined
+    null
+    obj
+
+    typeof() can be used for finding the types of variable instances
+    typeof (null) is object
+    typeof (undefined) if undefined
+*/
+
+
+
+
