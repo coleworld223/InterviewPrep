4cc0b8c5dd6d305c26080519d4e36cb7e85a3bc1
anuj <anujanand223@gmai.com>
2024-06-13 08:04:48 +0530

    potd

--- DEV/Javascript/tmp.js
+++ DEV/Javascript/tmp.js
@@ -9,6 +9,9 @@ var y = 5; // can be changed later / global scoped / can be redeclared
 // let x = 7; -> gives an error ("can not re initialise a let") 
 var y = 7; // no error
 
+/**************************************************************************************************************************/
+
+
 // Data Types 
 
 /*
@@ -23,6 +26,25 @@ var y = 7; // no error
     typeof() can be used for finding the types of variable instances
     typeof (null) is object
     typeof (undefined) if undefined
+*/
+
+/**************************************************************************************************************************/
+
+// Conversion
+
+let score = "33"
+let value = Number (score)
+console.log (typeof (score), typeof(value))
+console.log (score, value)
+
+/*
+    Type conversion can be done through (type with capital first letter)
+    e.g. Number ("33"), String (33), Boolean (1) etc.
+    score = 33bc -> val = NaN (Not a Number)
+    score = null -> val = 0
+    score = undefined -> value = NaN
+    score = 33 -> value = 33
+    NaN is a number type
 */
 
 
---
+++ GFG POTD/PadovanSeq.cpp
@@ -0,0 +1,32 @@
+/*
+    Hints:
+    This is a simple dyanamic problem just testing the basic fundamentals
+*/
+
+/*
+    Solution:
+    state : dp[i] = ith pavodan number
+    transition : dp[i] = dp[i - 2] + dp[i - 3]
+    
+    T.C -> O(n)
+    S.C -> O(n) -> O(1) (space optimisation)
+*/
+
+const int mod = 1e9 + 7;
+
+class Solution
+{
+    public:
+    int padovanSequence(int n) {
+        vector <int> dp (4);
+        dp[0] = dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i += 1) {
+            dp[3] = (dp[0] + dp[1]) % mod;
+            for (int j = 0; j < 3; j += 1) {
+                swap (dp[j], dp[j + 1]);
+            }
+        }
+        return dp[2];
+    }
+    
+};
